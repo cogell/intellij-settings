8c66c83353d02481b5bb2066bc06ad12fa6d4133
B. Cedric Cogell <cedric.cogell@gmail.com>
2016-09-05 13:40:05 -0400

    WS-2016.2.2 <ccogell13@us161347 Create baseRefactoring.xml, markdown.xml, Mac OS X 10_5_ copy.xml, IntelliLang.xml, ide.general.xml, Python.py, project.default.xml, diff.xml, user.xml, git.xml, other.xml, scratches.xml, Bash Script.sh, terminal.xml, file.template.settings.xml, ignore.xml, find.xml, filetypes.xml, code.style.schemes.xml, colors.scheme.xml, path.macros.xml, templates.xml, JavaScript File.js, Daylight.icls, Default.xml, JavaScript.xml, jdk.table.xml, Glacier.icls, laf.xml, Default _1_.xml, web-browsers.xml, abbreviations.xml, editor.codeinsight.xml, Markdown.md, keymap.xml, React Component.js, Dockerfile.xml, editor.xml, multimarkdown.shared.xml, applicationLibraries.xml, markdown_navigator.shared.xml, debugger.xml

---
+++ fileTemplates/React Component.js
@@ -0,0 +1,16 @@
+import React, { PropTypes, Component } from 'react';
+import styles from './styles.scss';
+
+const propTypes = {};
+
+const #[[$ComponentName$]]# = class extends Component {
+  render() {
+    return (
+      <span>#[[$ComponentName$]]#</span>
+    );
+  }
+};
+
+#[[$ComponentName$]]#.propTypes = propTypes;
+
+export default #[[$ComponentName$]]#;
